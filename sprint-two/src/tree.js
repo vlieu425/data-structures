7eb86b0117a775ea6bce55b54d2be8c0ff409fa4
vlieu425 <121201991+vlieu425@users.noreply.github.com>
2024-01-25 12:31:36 -0800

    complete implementation of the Tree class

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,10 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  //extend treeMethods onto newTree
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +14,31 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //create variable assign to new child node
+  var child = Tree(value);
+  //push new node to children array
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  //if current value === target
+  if (this.value === target) {
+    //return true
+    return true;
+  }
+
+  //loop through children array
+  for (var i = 0; i < this.children.length; i++) {
+    //if children array contains our target
+    // debugger;
+    if (this.children[i].contains(target)) {
+      //return true
+      return true;
+    }
+  }
+
+  //return false
+  return false;
 };
 
 
