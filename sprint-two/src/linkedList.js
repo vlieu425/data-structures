14f0c4b686e2f8ae0412e61d54790286817cad1c
vlieu425 <121201991+vlieu425@users.noreply.github.com>
2024-01-25 11:39:12 -0800

    complete implementation of LinkedList class

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,53 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //create new node variable and assign a call the Node class
+    var newNode = Node(value); //{3, null}
+    //if the linked list is empty
+    if (!list.head) {
+      //directly assign newNode to both head and tail
+      list.head = newNode;
+      list.tail = newNode;
+    //else
+    } else {
+      //current tail's next value needs to be reassigned to new nodes current value
+      list.tail.next = newNode;
+      //assign new node to list.tail
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    //if the linked list is empty
+    if (!list.head) {
+      //return null
+      return null;
+      //else
+    } else {
+      //create variable assign to current head
+      var currentHead = list.head;
+      //assign the head to next value
+      list.head = list.head.next;
+      //return current head variable
+      return currentHead.value;
+    }
   };
 
   list.contains = function(target) {
+    //create tracker variable, "increment" by assigning to the "next" value\
+    var currentNode = list.head;
+    //loop through linkedlist with while currentNode is not null
+    while (currentNode !== null) {
+      //if target equals currentnode's value
+      if (currentNode.value === target) {
+        // return true
+        return true;
+      }
+      //assign next node to currentNode
+      currentNode = currentNode.next;
+    }
+    //return false
+    return false;
   };
 
   return list;
@@ -21,7 +62,7 @@ var Node = function(value) {
   node.value = value;
   node.next = null;
 
-  return node;
+  return node;// {3}
 };
 
 /*
